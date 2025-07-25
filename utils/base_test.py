6b26ad3b7c5c9860cd61295dcf7b793b26adaa12
Thomas Sullivan <sullivantm15@gmail.com>
2025-07-25 10:39:03 -0700

    Update base_test.py
    Add code

--- utils/base_test.py
+++ utils/base_test.py
@@ -1,1 +1,10 @@
+from selenium import webdriver
+import pytest
+
+@pytest.fixture(scope="function")
+def setup():
+    driver = webdriver.Chrome()  # Or Firefox/Edge depending on what you have installed
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 

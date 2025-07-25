86273defcebcfaf495458cd050962132214bbe2b
Thomas Sullivan <sullivantm15@gmail.com>
2025-07-25 10:40:37 -0700

    Update tests_login.py
    Add code

--- tests/tests_login.py
+++ tests/tests_login.py
@@ -1,1 +1,17 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.ID, "txtUsername")
+        self.password_input = (By.ID, "txtPassword")
+        self.login_button = (By.ID, "btnLogin")
+
+    def load(self, url="https://opensource-demo.orangehrmlive.com/"):
+        self.driver.get(url)
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
 
